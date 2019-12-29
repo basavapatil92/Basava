4eba22a195d4dbccccca103268f3cd5a12183349
basavapatil92 <56518011+basavapatil92@users.noreply.github.com>
2019-12-29 22:30:57 +0530

    Add files via upload
    Java Files

---
+++ TestExample.java
@@ -0,0 +1,11 @@
+class TestExample
+{
+	public static void main(String []args)
+	{
+		System.out.println("Java programs are uploaded");
+		for(int i=0;i<=5;i++)
+		{
+			System.out.println("Missing U");
+		}
+	}
+}
